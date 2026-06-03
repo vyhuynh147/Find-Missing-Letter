d7f3e94129b28cd2fbd434f2f6d22f3548d19625
vyhuynh147 <huynhyenvy140709@gmail.com>
2026-06-03 13:41:18 -0400

    working

--- main.py
+++ main.py
@@ -1,230 +1,229 @@
-# # import module
-# import os
-# import random
-# import time
-# import sys
+# import module
+import os
+import random
+import time
+import sys
 
-# os.system('cls||clear')
-# #typing function 
-# def typing(text,speed =0.03):
-#     for  char in text:
-#         sys.stdout.write(char)
-#         sys.stdout.flush()
-#         time.sleep(speed)
-#     print()
-
-
-# # typing welcome
-# typing(" Welcome to Find Missing Word!!\n")
-# typing("-> Your goal is to find missing the word in each sentence. ")
-# typing("   Choose the correct word in the list.")
-# typing("   When playing the game you will be able to have a streak and you will recevie hints.")
-# typing("   Have fun practicing!!! \n")
-
-# # ask the user name
-# typing("Enter Your Name Below:")
-# user_name = input(">").strip()
-# # print()
-
-# typing(f"Hey {user_name}, are you ready for pratice? (y/n): ")
-# ready = input(">").strip().lower()
-
-# if ready != "y":
-#     typing("Okay! see you next time")
-#     time.sleep(1.5)
-#     os.system('cls||clear')
-#     sys.exit()
+os.system('cls||clear')
+#typing function 
+def typing(text,speed =0.005):
+    for  char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(speed)
+    print()
 
 
-# os.system('cls||clear')
-# typing("\n Let's try to do some sentences for practing!")
+# typing welcome
+typing(" Welcome to Find Missing Word!!\n")
+typing("-> Your goal is to find missing the word in each sentence. ")
+typing("   Choose the correct word in the list.")
+typing("   When playing the game you will be able to have a streak and you will recevie hints.")
+typing("   Have fun practicing!!! \n")
+
+# ask the user name
+typing("Enter Your Name Below:")
+user_name = input(">").strip()
+
+typing(f"Hey {user_name}, are you ready for pratice? (y/n): ")
+ready = input(">").strip().lower()
+
+if ready != "y":
+    typing("Okay! see you next time")
+    time.sleep(1.5)
+    os.system('cls||clear')
+    sys.exit()
 
 
-# #Easy part
-# # make lists the words for each level: Easy, Medium, Hard
-# E_words = ["chopsticks", "sat", "eating","lost","everyday","tricks","come","programmer","fishing","crescent"]
-
-# # make a list for easy sentences.
-# E_sentences = ["He eats rice with __________.",
-#                "The cat ___ in the chair.",
-#                "She was ______ and talking.",
-#                "I ____ my watch yesterday.",
-#                "He does not go to the gym ________.",
-#                "You don't teach your cat ______.",
-#                "No one will ____ after me.",
-#                "I want to be a computer __________.",
-#                "We went _______ after school.",
-#                "The moon looks like a ________."]
-
-# # make a dictionary for the answer
-# E_answers = {
-#     E_sentences[0]:"chopsticks",
-#     E_sentences[1]:"sat",
-#     E_sentences[2]:"eating",
-#     E_sentences[3]:"lost",
-#     E_sentences[4]:"everyday",
-#     E_sentences[5]:"tricks",
-#     E_sentences[6]:"come",
-#     E_sentences[7]:"programmer",
-#     E_sentences[8]:"fishing",
-#     E_sentences[9]:"crescent"
-#     }
-
-# # Medium part
-# M_words = ["docked","managed","passionate","barn","discuss","should","fascinating","afraid","mentioning","umbrella"] 
-
-# M_sentences = ["If he was late again, he would be ______ a day's pay.",
-#                "Even though she was late, she still _______ to finish her work.",
-#                "While I am a __________ basketball fan, but I prefer soccer.",
-#                "The horse raced past the ____.",
-#                "Let's schedule a meeting to _______ the project details.",
-#                "It was nice to talking to you; we ______ do this again.",
-#                "The teacher, who is known for being strict, gave a ___________ lecture.",
-#                "I'm ______ I cannot attend the meeting on Friday.",
-#                "Speak of the devil, we were just __________ your recent promotion.",
-#                "It's going to rain this afternoon, so we should bring an ________."]
-
-# M_answers = {
-#     M_sentences[0]:"docked",
-#     M_sentences[1]:"managed",
-#     M_sentences[2]:"passionate",
-#     M_sentences[3]:"barn",
-#     M_sentences[4]:"discuss",
-#     M_sentences[5]:"should",
-#     M_sentences[6]:"fascinating",
-#     M_sentences[7]:"afraid",
-#     M_sentences[8]:"mentioning",
-#     M_sentences[9]:"umbrella"
-# }
+os.system('cls||clear')
+typing("\n Let's try to do some sentences for practing!")
 
 
-# # hard part
-# H_words = ["untangling","ambiguous","meticulous","reluctant","comprehensive","resilient","groundbreaking","arrogant","intricate","profound"]
+#Easy part
+# make lists the words for each level: Easy, Medium, Hard
+E_words = ["chopsticks", "sat", "eating","lost","everyday","tricks","come","programmer","fishing","crescent"]
 
-# H_sentences = ["The electrical wires require a lot of __________.",
-#                "The politician's speech was full of _________ statements that confused the audience.",
-#                "Her __________ attention to detail made the project nearly perfect.",
-#                "His _________ agreement showed that he wasn't fully convinced.",
-#                "The teacher gave a _____________ explanation of the complex topic.",
-#                "She was known for her _________ attitude, never giving up despite challenges.",
-#                "The scientist made a ______________ discovery that changed modern medicine.",
-#                "His ________ behavior made it difficult for others to work with him.",
-#                "The instructions were so _________ that many people struggled to follow them.",
-#                "The novel presents a ________ message about life and human nature."]
+# make a list for easy sentences.
+E_sentences = ["He eats rice with __________.",
+               "The cat ___ in the chair.",
+               "She was ______ and talking.",
+               "I ____ my watch yesterday.",
+               "He does not go to the gym ________.",
+               "You don't teach your cat ______.",
+               "No one will ____ after me.",
+               "I want to be a computer __________.",
+               "We went _______ after school.",
+               "The moon looks like a ________."]
 
-# H_answers = {
-#     H_sentences[0]:"untangling",
-#     H_sentences[1]:"ambiguous",
-#     H_sentences[2]:"meticulous",
-#     H_sentences[3]:"reluctant",
-#     H_sentences[4]:"comprehensive",
-#     H_sentences[5]:"resilient",
-#     H_sentences[6]:"groundbreaking",
-#     H_sentences[7]:"arrogant",
-#     H_sentences[8]:"intricate",
-#     H_sentences[9]:"profound",
-# }
+# make a dictionary for the answer
+E_answers = {
+    E_sentences[0]:"chopsticks",
+    E_sentences[1]:"sat",
+    E_sentences[2]:"eating",
+    E_sentences[3]:"lost",
+    E_sentences[4]:"everyday",
+    E_sentences[5]:"tricks",
+    E_sentences[6]:"come",
+    E_sentences[7]:"programmer",
+    E_sentences[8]:"fishing",
+    E_sentences[9]:"crescent"
+    }
 
-# # Use function to run the game
-# def play_game(level):
-# # loop to run the whole game 
-#     score = 0
-#     streak = 0
-#     max_attempts = 2
-#     attempts = 0
+# Medium part
+M_words = ["docked","managed","passionate","barn","discuss","should","fascinating","afraid","mentioning","umbrella"] 
 
-#     while True: 
-#         # loop for select level
-#         while True:
-#             if level == "easy":
-#                 words = E_words
-#                 sentences = E_sentences
-#                 answers = E_answers
-#                 break
+M_sentences = ["If he was late again, he would be ______ a day's pay.",
+               "Even though she was late, she still _______ to finish her work.",
+               "While I am a __________ basketball fan, but I prefer soccer.",
+               "The horse raced past the ____.",
+               "Let's schedule a meeting to _______ the project details.",
+               "It was nice to talking to you; we ______ do this again.",
+               "The teacher, who is known for being strict, gave a ___________ lecture.",
+               "I'm ______ I cannot attend the meeting on Friday.",
+               "Speak of the devil, we were just __________ your recent promotion.",
+               "It's going to rain this afternoon, so we should bring an ________."]
 
-#             elif level == "medium":
-#                 words = M_words
-#                 sentences = M_sentences
-#                 answers = M_answers
-#                 break
+M_answers = {
+    M_sentences[0]:"docked",
+    M_sentences[1]:"managed",
+    M_sentences[2]:"passionate",
+    M_sentences[3]:"barn",
+    M_sentences[4]:"discuss",
+    M_sentences[5]:"should",
+    M_sentences[6]:"fascinating",
+    M_sentences[7]:"afraid",
+    M_sentences[8]:"mentioning",
+    M_sentences[9]:"umbrella"
+}
 
-#             elif level == "hard":
-#                 words = H_words
-#                 sentences = H_sentences
-#                 answers = H_answers
-#                 break 
-#             else:
-#                 typing(" Invalid level!")
 
-#         sentence = random.choice(sentences)
-#         correct = answers[sentence]
+# hard part
+H_words = ["untangling","ambiguous","meticulous","reluctant","comprehensive","resilient","groundbreaking","arrogant","intricate","profound"]
 
-#         typing(f"Word list: {words}")
-#         typing(f"\nHere is the sentence: ")
-#         typing(sentence)
+H_sentences = ["The electrical wires require a lot of __________.",
+               "The politician's speech was full of _________ statements that confused the audience.",
+               "Her __________ attention to detail made the project nearly perfect.",
+               "His _________ agreement showed that he wasn't fully convinced.",
+               "The teacher gave a _____________ explanation of the complex topic.",
+               "She was known for her _________ attitude, never giving up despite challenges.",
+               "The scientist made a ______________ discovery that changed modern medicine.",
+               "His ________ behavior made it difficult for others to work with him.",
+               "The instructions were so _________ that many people struggled to follow them.",
+               "The novel presents a ________ message about life and human nature."]
 
-#         user = input("Fill the blank of the answer: ").strip().lower()
+H_answers = {
+    H_sentences[0]:"untangling",
+    H_sentences[1]:"ambiguous",
+    H_sentences[2]:"meticulous",
+    H_sentences[3]:"reluctant",
+    H_sentences[4]:"comprehensive",
+    H_sentences[5]:"resilient",
+    H_sentences[6]:"groundbreaking",
+    H_sentences[7]:"arrogant",
+    H_sentences[8]:"intricate",
+    H_sentences[9]:"profound",
+}
 
-#         # user get right, increase score ,streak
-#         if user == correct:
-#             score += 1
-#             streak += 1
-#             attempts = 0
-#             typing(f"Yes, you got it {user_name}!!!")
-#             typing(f" Your score is: {score} )
-#             # typing(f " Your streak is: {streak})
+# Use function to run the game
+def play_game(level):
+# loop to run the whole game 
+    score = 0
+    streak = 0
+    max_attempts = 2
+    attempts = 0
 
-#             # # time.sleep(1.5)
-#             # os.system('cls||clear')
-#         else:
-#             score -= 1 
-#             attempts += 1
-#             streak = 0
+    while True: 
+        # loop for select level
+        while True:
+            if level == "easy":
+                words = E_words
+                sentences = E_sentences
+                answers = E_answers
+                break
+
+            elif level == "medium":
+                words = M_words
+                sentences = M_sentences
+                answers = M_answers
+                break
+
+            elif level == "hard":
+                words = H_words
+                sentences = H_sentences
+                answers = H_answers
+                break 
+            else:
+                typing(" Invalid level!")
+
+        sentence = random.choice(sentences)
+        correct = answers[sentence]
+
+        typing(f"Word list: {words}")
+        typing(f"\nHere is the sentence: ")
+        typing(sentence)
+
+        user = input("Fill the blank of the answer: ").strip().lower()
+
+        # user get right, increase score ,streak
+        if user == correct:
+            score += 1
+            streak += 1
+            attempts = 0
+            typing(f"Yes, you got it {user_name}!!!")
+            typing(f" Your score is: {score} ")
+            typing(f" Your streak is: {streak}")
+
+            # # time.sleep(1.5)
+            # os.system('cls||clear')
+        else:
+            score -= 1 
+            attempts += 1
+            streak = 0
             
-#             typing(" You're wrong, but it's okay!")
-#             # time.sleep(1.5)
-#             # os.system('cls||clear')
+            typing(" You're wrong, but it's okay!")
+            # time.sleep(1.5)
+            # os.system('cls||clear')
 
 
-#         # ask the user for hints. 
-#             use_hint = input("Do you want to use a hint? (yes/ no ): ").strip().lower()
+        # ask the user for hints. 
+            use_hint = input("Do you want to use a hint? (yes/ no ): ").strip().lower()
         
-#             if use_hint == "yes":
-#                 typing("Hint: The words starts with",correct[0] )
+            if use_hint == "yes":
+                typing(f"Hint: The words starts with {correct[0]}" )
 
-#                 user = input(" Let's try again: ").strip().lower()
+                user = input(" Let's try again: ").strip().lower()
 
 
-#         #second chance if they get right
-#                 if user == correct:
-#                     score +=1
-#                     streak +=1
-#                     attempts =0
-#                     typing(" Yes, you got it right this time!!!")
-#                 else:
-#                     attempts +=1
-#                     typing(" Oh no, still wrong.")
+        #second chance if they get right
+                if user == correct:
+                    score +=1
+                    streak +=1
+                    attempts =0
+                    typing(" Yes, you got it right this time!!!")
+                else:
+                    attempts +=1
+                    typing(" Oh no, still wrong.")
             
-#         # show the answer if final answer is wrong
-#         if user != correct:
-#             typing("\n The correct answer was", correct)
+        # show the answer if final answer is wrong
+        if user != correct:
+            typing(f"\n The correct answer was {correct}")
 
 
-#         if attempts >= max_attempts:
-#             streak = 0
-#             attempts = 0
-#             typing(" You got it wrong! Your streak has been reset.")
+        if attempts >= max_attempts:
+            streak = 0
+            attempts = 0
+            typing(" You got it wrong! Your streak has been reset.")
                     
-#         # typing all the current score and streak when they use max attempts. 
-#         typing("\n>> Your current score is:", score)
-#         typing(" >> Your current streak is:", streak)
+        # typing all the current score and streak when they use max attempts. 
+        typing(f"\n>> Your current score is: {score}")
+        typing(f" >> Your current streak is: {streak}")
 
 
-# # ask the user want to play again
-#         more = input("Do you want another sentence?(yes/no): ").strip().lower()
-#         if more != "yes":
-#             typing("Thanks you for play Find Missing Words.")
-#             break 
+# ask the user want to play again
+        more = input("Do you want another sentence?(yes/no): ").strip().lower()
+        if more != "yes":
+            typing("Thanks you for play Find Missing Words.")
+            break 
 
-# level = input("(easy, medium, hard) Let's choose level you want to select: ").strip().lower()
-# play_game(level)
+level = input("(easy, medium, hard) Let's choose level you want to select: ").strip().lower()
+play_game(level)
